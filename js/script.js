beee2b99b61f84b955aa5a8fa4352018c840b6fb
Natália Campos <nataliacamposnccs@gmail.com>
2022-11-18 17:00:52 -0300

    creating divs

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
+const header = document.createElement('header');
+document.body.appendChild(header);
+
 const main = document.createElement("main");
 document.body.appendChild(main);
+
+const footer = document.createElement('footer');
+document.body.appendChild(footer);
+
+const firstDiv = document.createElement('div');
+main.appendChild(firstDiv);
 
 const titulo = document.createElement("h1");
 const btnDiv = document.createElement("div");
@@ -7,8 +16,8 @@ const btnEstab = document.createElement("button");
 const btnCateg = document.createElement("button");
 const btnGrupo = document.createElement("button");
 
-main.appendChild(titulo);
-main.appendChild(btnDiv);
+firstDiv.appendChild(titulo);
+firstDiv.appendChild(btnDiv);
 btnDiv.appendChild(btnEstab);
 btnDiv.appendChild(btnCateg);
 btnDiv.appendChild(btnGrupo);
@@ -21,15 +30,45 @@ btnGrupo.textContent = "Grupos";
 const pagEstab = document.createElement("div");
 const pagCateg = document.createElement("div");
 const pagGrupo = document.createElement("div");
-
-main.appendChild(pagEstab);
-main.appendChild(pagCateg);
-main.appendChild(pagGrupo);
-
-
-
-
+pagEstab.classList.add("pagEstab");
+pagCateg.classList.add("pagCateg");
+pagGrupo.classList.add("pagGrupo");
+firstDiv.appendChild(pagEstab);
+firstDiv.appendChild(pagCateg);
+firstDiv.appendChild(pagGrupo);
 
 
 
+const lblEstab = document.createElement("h2");
+lblEstab.textContent = "Estabelecimentos cadastrados";
+pagEstab.appendChild("lblEstab");
+
+// window.addEventListener('scroll', () => {
+//     const $window = $(window);
+//     const nav = $('.header');
+
+//     $window.scroll(function() {
+//         if ($window.scrollTop() >= '100vh'){
+//             header.style.display = "none";
+//             footer.style.display = "none";
+//         }
+//         else{
+//             header.style.display = "block";
+//             footer.style.display = "block";   
+//         }
+//     })
+
+// })
+
+
+window.onscroll = function() {scrollTop()};
+let sticky = header.offsetTop;
+
+function scrollTop() {
+    if (window.pageYOffset > sticky) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+}
 
--- js/styles.js
+++ js/styles.js
@@ -1,6 +1,17 @@
+Object.assign(document.body.style, {
+    margin: "0",
+    padding: "0",
+})
+
 Object.assign(main.style, {
     width: "95vw",
-    height: "95vh",
+    height: "400vh"
+})
+
+
+Object.assign(firstDiv.style, {
+    width: "95vw",
+    height: "135%",
     border: "2px solid red",
     display: "flex",
     flexDirection: "column",
@@ -45,3 +56,43 @@ Object.assign(btnGrupo.style, {
     cursor: "pointer",
 })
 
+Object.assign(header.style, {
+    display: "none",
+    position: "fixed",
+    top: "0",
+    left: "0",
+    background: "black",
+    width: "95vw",
+    height: "10vh"
+})
+
+Object.assign(footer.style, {
+    display: "none",
+    background: "black",
+    position: "fixed",
+    bottom: "0",
+    left: "0",
+    width: "95vw",
+    height: "10vh"
+})
+
+
+Object.assign(pagEstab.style, {
+    width: "95vw",
+    height: "100%",
+    background: "red",
+    display: "flex",
+    flexDirection: "column",
+})
+
+Object.assign(pagCateg.style, {
+    width: "95vw",
+    height: "100%",
+    background: "blue"
+})
+
+Object.assign(pagGrupo.style, {
+    width: "95vw",
+    height: "100%",
+    background: "green"
+})
