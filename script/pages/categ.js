e43d6b280261112c22c480133479ec6def292b9b
carvalhosmari <mscarvall@gmail.com>
2022-11-21 14:48:59 -0300

    modelo de tabela criado

--- script/pages/categ.js
+++ script/pages/categ.js
@@ -1,18 +1,54 @@
 
-window.Page.categPage = () => {
+window.Page.categPage = async async => {
     main.innerHTML = '';
 
     const headerContent = ["inicio", "estabelecimentos", "categorias", "grupo"];
-    const hdr =  header.create(headerContent)
+    const hdr = header.create(headerContent)
     main.appendChild(hdr)
     hdr.addEventListener('scroll', () => {
-        
+
     })
 
     const section = document.createElement('section');
     main.appendChild(section);
-    
-    section.appendChild(
+
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    const tfoot = document.createElement('tfoot');
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    table.appendChild(tfoot);
+
+    section.appendChild(table);
+
+    const headerTable = ["Código", "Nome", "Editar", "Deletar"];
+
+    for (let i = 0; headerTable.length; i++) {
+        const th = document.createElement('th');
+        th.textContent = headerTable[i];
+
+        thead.appendChild(th);
+    }
+
+    const categories = await getCategories();
+
+    categories.forEach(categoria => {
+
+        const trObj = document.createElement('tr');
+        Object.keys(categoria).forEach(key => {
+            if (key != "uid") {
+                let tdAtributosObj = document.createElement('td');
+                tdAtributosObj.innerText = categoria[key];
+                trObj.appendChild(tdAtributosObj);
+            }
+        })
+
+        tbody.appendChild(trObj);
+    });
+
+    tfoot.appendChild(
         btn.create({
             content: "Cadastrar categoria",
             onClick: () => {
@@ -21,20 +57,4 @@ window.Page.categPage = () => {
             className: 'btn-register'
         })
     )
-    
-    // const table = document.createElement('table');
-    // section.appendChild(table);
-
-    // const atributos = [];
-    // getCategories().array.forEach(categoria => {
-
-    //     const trObj = document.createElement('tr');
-    //     Object.keys(categoria).forEach(key =>{
-    //         let tdAtributosObj = document.createElement('td');
-    //         tdAtributosObj.innerText = categoria[key];
-    //         trObj.appendChild(tdAtributosObj);
-    //     })
-
-    //     table.appendChild(trObj);
-    // });
 }
