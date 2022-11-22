1881e1ffa365a45b43367381a73a54c233f9453d
Natália Campos <nataliacamposnccs@gmail.com>
2022-11-22 10:35:15 -0300

    get row code

--- script/pages/categ.js
+++ script/pages/categ.js
@@ -1,52 +1,93 @@
 
-window.Page.categPage = async () => {
+window.Page.categPage = async async => {
     main.innerHTML = '';
 
     const headerContent = ["inicio", "estabelecimentos", "categorias", "grupo"];
     const hdr = header.create(headerContent)
-    main.appendChild(hdr);
-    
+    main.appendChild(hdr)
+    hdr.addEventListener('scroll', () => {
+
+    })
+
     const section = document.createElement('section');
-    section.classList.add('section-list');
     main.appendChild(section);
+
+    const table = document.createElement('table');
+    const thead = document.createElement('thead');
+    const tbody = document.createElement('tbody');
+    const tfoot = document.createElement('tfoot');
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    table.appendChild(tfoot);
+
+    section.appendChild(table);
 
     const headerTable = ["Codigo", "Nome", "Editar", "Deletar"];
 
+    headerTable.forEach(e => {
+        const th = document.createElement('th');
+        th.textContent = e;
+        thead.appendChild(th)
+    })
+
     const categories = await getCategories();
 
-    section.appendChild(
-        tableDisplay.create(headerTable, categories)
-    );
+    categories.forEach(categoria => {
 
-    const iconEdit = document.querySelectorAll('.icon-edit');
+        const trObj = document.createElement('tr');
+        Object.keys(categoria).forEach(key => {
+            if (key != "uid") {
+                let tdAtributosObj = document.createElement('td');
+                tdAtributosObj.innerText = categoria[key];
+                trObj.appendChild(tdAtributosObj);
+            }
+        })
+        
+        const td1 = document.createElement('td');
+        const td2 = document.createElement('td');
 
-    iconEdit.forEach(icon => {
-        icon.addEventListener('click', () => {
-            Page.categEdit();
+        const icon1 = document.createElement('img');
+        const icon2 = document.createElement('img');
+        icon1.setAttribute('src', './img/edit.png');
+        icon2.setAttribute('src', './img/delete.png');
+
+        icon1.addEventListener('click', () => {
+            let rIndex;
+
+            for (let i = 0; i < table.rows.length; i++) {
+                table.rows[i].onclick = function(){
+                    rIndex = this.rowIndex;
+                    console.log(rIndex);
+                }
+            }
+
+            console.log(rIndex + "vou poder usar fora");
+
         });
-    
-    })
 
-    const iconDelete = document.querySelectorAll('.icon-delete');
-    iconDelete.forEach(icon => {
-        icon.addEventListener('click', () => {
-            Page.mainPage();
-        });
-    
-    })
+        icon2.addEventListener('click', () => {
+            console.log("Row index: " + cell.closest('tr').rowIndex + " | Column index: " + cell.cellIndex);
+        })
 
-    const tfoot = document.querySelector('.table-footer');
+        td1.appendChild(icon1);
+        td2.appendChild(icon2);
+
+        trObj.appendChild(td1);
+        trObj.appendChild(td2);
+
+        
+
+        tbody.appendChild(trObj);
+    });
+
     tfoot.appendChild(
         btn.create({
             content: "Cadastrar categoria",
             onClick: () => {
-                Page.categRegister()
+                Page.categRegister();
             },
             className: 'btn-register'
         })
-    );
-
-    main.appendChild(
-        footer.create()
-    );
+    )
 }
